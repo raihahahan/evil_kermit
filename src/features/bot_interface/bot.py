6e25e27c93fb61061d1796a5c19fd149ee384c99
raihahahan <mraihandev@gmail.com>
2024-01-20 11:51:50 +0800

    added requirements.txt

--- src/features/bot_interface/bot.py
+++ src/features/bot_interface/bot.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 import os
 from telegram.ext import ApplicationBuilder, CommandHandler
 import logging 
-from features.bot_interface import bot_utils
+from features.bot_interface import bot_command_handlers
 
 load_dotenv()
 
@@ -11,15 +11,19 @@ logging.basicConfig(
     level=logging.INFO
 )
 
-
-
 class Bot():
     def __init__(self):
         self.bot = ApplicationBuilder().token(os.getenv("BOT_API")).build()
 
     def run(self):
-        start_handler = CommandHandler('start', bot_utils.start)
+        start_handler = CommandHandler('start', bot_command_handlers.start)
+        clone_handler = CommandHandler('clone', bot_command_handlers.clone)
+
+        # add handlers
         self.bot.add_handler(start_handler)
+        self.bot.add_handler(clone_handler)
+
+        # start polling
         self.bot.run_polling()
 
 
--- src/features/bot_interface/bot_utils.py
+++ src/features/bot_interface/bot_utils.py
@@ -1,5 +1,17 @@
-from telegram.ext import ContextTypes
-from telegram import Update
+import re
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="Bot is up and running.")
+def extract_telegram_handle(input_string):
+    # Define a regular expression pattern to match the Telegram handle without the @ symbol
+    pattern = r'/clone @(\w+)'
+
+    # Use re.match to search for the pattern in the input string
+    match = re.match(pattern, input_string)
+
+    # Check if a match is found
+    if match:
+        # Extract the Telegram handle from the matched group
+        telegram_handle = match.group(1)
+        return telegram_handle
+    else:
+        # Return None if no match is found
+        return None
